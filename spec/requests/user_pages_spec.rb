87a6b405d80444f11bab61b569320c5c98950727
slapappy <timkelly@timkelly.org>
2013-08-20 13:31:41 -0700

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -29,6 +29,12 @@ describe "User pages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+
+      describe "after submission" do
+        before { click_button submit }
+        it { should have_content( 'error' ) }
+        it { should have_title ( 'Sign Up' ) }
+      end
     end
 
     describe "with valid information" do
@@ -39,8 +45,20 @@ describe "User pages" do
         fill_in "Confirmation", with: "foobar"
       end
 
+      
+
+    #it "should display an error message" do
+    #    expect { click_button submit}.not_to
+    #end
+
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+      describe "after saving the user" do
+        before { click_button submit } 
+        let(:user) { User.find_by(email: 'user@example.com') }
+        it { should have_title(user.name) }
+        it { should have_selector( 'div.alert.alert-success', text: 'Welcome' ) }
       end
     end
   end
